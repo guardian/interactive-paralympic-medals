9c300a5df30d01defac20eb02f31bb2c6de5638d
cfen <chris.fenn@guardian.co.uk>
2016-09-07 11:08:19 +0100

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import reqwest from 'reqwest'
+import mainHTML from './text/main.html!text'
+import share from './lib/share'
+
+var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
+
+export function init(el, context, config, mediator) {
+    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+    reqwest({
+        url: 'https://interactive.guim.co.uk/docsdata-test/1bZnrqdBqM1jFuC-iBpOV-_lC8eE2uwb1g1Z3FTy6UJA.json',
+        type: 'json',
+        crossOrigin: true,
+        success: (resp) => initData(resp)
+    });
+
+    [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
+        var network = shareEl.getAttribute('data-network');
+        shareEl.addEventListener('click',() => shareFn(network));
+    });
+}
+
+
+function initData(resp){
+    console.log(resp.sheets.Sheet1)
+}
